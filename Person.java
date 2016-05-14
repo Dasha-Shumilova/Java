301b20c4e56a23a2f33ffc7e99b456f71d96d7bf
Dasha-Shumilova <dasha-shumilova@lenta.ru>
2016-05-14 03:00:50 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -32,4 +32,62 @@ public abstract class Person {
         this.age = age;
         this.isMale = isMale;
     }
+     public Person(){}
+
+    public String toJson() {
+        return "{\"name\":" + "\"" + name + "\"" + "," + "\"age\":" + "\"" + age + "\"" + ","
+                + "\"isMale\":" + isMale + "}";
+    }
+
+
+    public void writeToFile(String filePath) throws IOException {
+        Writer writer = null;
+
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(filePath), "utf-8"));
+            writer.write(this.toJson());
+        } finally {
+            writer.close();
+        }
+    }
+
+    public void readFromFile(String url) {
+        try {
+
+            FileReader reader = new FileReader(url);
+            BufferedReader in = new BufferedReader(reader);
+            String string;
+            while ((string = in.readLine()) != null) {
+                System.out.println(string);
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+      public void FromJson(String filePath) {
+      JSONParser parser = new JSONParser();
+
+       try {
+
+
+           Object obj = this.getParser().parse(new FileReader(filePath));
+           JSONObject jsonObject = (JSONObject) obj;
+
+           String name = (String) jsonObject.get("name");
+           Long age = (Long) jsonObject.get("age");
+           boolean isMale = (boolean) jsonObject.get("isMale");
+
+           this.setName(name);
+           this.setAge(age.intValue());
+           this.setIsMale(isMale);
+           System.out.println("\n--------------------------\n" + "Студент " + name + "\n Bік:" + age +"\n");
+
+
+        } catch (Exception e) {
+        e.printStackTrace();
+        }
+   }
 }
